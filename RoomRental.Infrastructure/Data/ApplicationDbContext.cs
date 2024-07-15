9da6bd3d1a16053f49cdbcffcd4bbfb6c25abb1b
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-15 20:56:48 +0300

    Create Villa Table

--- RoomRental.Infrastructure/Data/ApplicationDbContext.cs
+++ RoomRental.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RoomRental.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace RoomRental.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+        public DbSet<Villa> Villas { get; set; }
     }
 }
