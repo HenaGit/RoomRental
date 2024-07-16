fc55a074cb010816c235a1811761548f66984fc1
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-16 16:52:54 +0300

    Add TempData Notifications

--- RoomRental.Web/Controllers/VillaController.cs
+++ RoomRental.Web/Controllers/VillaController.cs
@@ -33,6 +33,7 @@ namespace RoomRental.Web.Controllers
             {
                 _db.Villas.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -55,6 +56,7 @@ namespace RoomRental.Web.Controllers
             {
                 _db.Villas.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
                 return RedirectToAction("Index");
             }
             return View();
@@ -76,6 +78,7 @@ namespace RoomRental.Web.Controllers
             {
                 _db.Villas.Remove(objFromDb);
                 _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
                 return RedirectToAction("Index");
             }
             return View();
