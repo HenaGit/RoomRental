f4091e7c2e09c600e39dd12bb2fe891947a5ba8a
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-16 18:11:12 +0300

    Villa Number Create View

--- RoomRental.Domain/Entities/VillaNumber.cs
+++ RoomRental.Domain/Entities/VillaNumber.cs
@@ -11,6 +11,7 @@ namespace RoomRental.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Villa Number")]
         public int Villa_Number { get; set; }
 
         [ForeignKey("Villa")]
