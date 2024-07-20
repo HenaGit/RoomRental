ef1d160a3fa7fb0e891bca871285624eb71cf144
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-20 23:52:06 +0300

    Amenity Repository

---
+++ RoomRental.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,26 @@
+﻿using RoomRental.Application.Common.Interfaces;
+using RoomRental.Domain.Entities;
+using RoomRental.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomRental.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+}
