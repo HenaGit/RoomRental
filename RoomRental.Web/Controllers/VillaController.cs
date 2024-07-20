0fdbfd7a414a49e6904738082500ef86cc02dd2a
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-20 23:33:16 +0300

    Remove Image on Villa Delete

--- RoomRental.Web/Controllers/VillaController.cs
+++ RoomRental.Web/Controllers/VillaController.cs
@@ -113,6 +113,15 @@ namespace RoomRental.Web.Controllers
             Villa? objFromDb = _unitOfWork.Villa.Get(u => u.Id == obj.Id);
             if (objFromDb is not null)
             {
+                if (!string.IsNullOrEmpty(objFromDb.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
                 _unitOfWork.Villa.Remove(objFromDb);
                 _unitOfWork.Save();
                 TempData["success"] = "The villa has been deleted successfully.";
