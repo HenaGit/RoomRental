8e38018e69ddf9ada3bc82d12414334ff827e188
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-21 23:55:15 +0300

    Add Villa Availability

--- RoomRental.Domain/Entities/Villa.cs
+++ RoomRental.Domain/Entities/Villa.cs
@@ -30,5 +30,7 @@ namespace RoomRental.Domain.Entities
         public DateTime? Updated_Date { get; set; }
         [ValidateNever]
         public IEnumerable<Amenity> VillaAmenity { get; set; }
+        [NotMapped]
+        public bool IsAvailable { get; set; } = true;
     }
 }
