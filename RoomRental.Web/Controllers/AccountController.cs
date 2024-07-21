be5317d5ca97ad7e4b788121c9e06c3da031cf51
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-21 19:49:11 +0300

    Retrieve Roles in Dropdown

--- RoomRental.Web/Controllers/AccountController.cs
+++ RoomRental.Web/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RoomRental.Application.Common.Interfaces;
 using RoomRental.Domain.Entities;
 using RoomRental.Web.ViewModels;
@@ -42,7 +43,16 @@ namespace RoomRental.Web.Controllers
                 _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
                 _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
             }
-            return View();
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+
+            return View(registerVM);
         }
     }
 }
