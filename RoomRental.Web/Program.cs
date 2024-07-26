0898952aab47760f0ca1cb57fb881123d27c85a6
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-26 21:22:33 +0300

    Add Stripe Keys

--- RoomRental.Web/Program.cs
+++ RoomRental.Web/Program.cs
@@ -4,10 +4,10 @@ using RoomRental.Application.Common.Interfaces;
 using RoomRental.Domain.Entities;
 using RoomRental.Infrastructure.Data;
 using RoomRental.Infrastructure.Repository;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -26,11 +26,11 @@ builder.Services.Configure<IdentityOptions>(option =>
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
