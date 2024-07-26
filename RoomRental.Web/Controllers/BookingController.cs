b29c4755fd4dbba0ea6b5122f8b569666d14be39
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-07-26 23:49:02 +0300

    Add Status filter in action

--- RoomRental.Web/Controllers/BookingController.cs
+++ RoomRental.Web/Controllers/BookingController.cs
@@ -112,7 +112,7 @@ namespace RoomRental.Web.Controllers
         #region API Calls
         [HttpGet]
         [Authorize]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
             IEnumerable<Booking> objBookings;
 
@@ -127,6 +127,10 @@ namespace RoomRental.Web.Controllers
 
                 objBookings = _unitOfWork.Booking
                     .GetAll(u => u.UserId == userId, includeProperties: "User,Villa");
+            }
+            if (!string.IsNullOrEmpty(status))
+            {
+                objBookings = objBookings.Where(u => u.Status.ToLower().Equals(status.ToLower()));
             }
             return Json(new { data = objBookings });
         }
